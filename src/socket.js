b79e870f132a35cfa8334397e285c25d892cea7c
gabrieleeh32159 <gabriel.espinoza.h@utec.edu.pe>
2024-03-09 02:02:33 -0500

    Fix production url connection

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'https://real-time-chat-backend-production.up.railway.app' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? "https://real-time-chat-backend-production.up.railway.app" : 'http://localhost:3000';
 
 export const socket = io(URL);
